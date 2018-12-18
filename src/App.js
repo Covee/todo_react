3435f7e206b76a86ca0de6f9d42e715ac0cf8a28
Cobee <uniqtop91@gmail.com>
2018-12-18 17:54:02 +0900

    nothing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import PageTemplate from './components/PageTemplate';
+import TodoInput from './components/TodoInput';
 
 
 class App extends Component {
     render() {
         return (
-            <PageTemplate>Hi!</PageTemplate>
+            <PageTemplate>
+                <TodoInput />
+            </PageTemplate>
             
         )
     }
