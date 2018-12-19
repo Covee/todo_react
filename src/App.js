509f9c6447648b6fe3624843ad8645d11691062f
Cobee <uniqtop91@gmail.com>
2018-12-19 10:55:25 +0900

    TodoList outline finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PageTemplate from './components/PageTemplate';
 import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList/TodoList';
 
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
         return (
             <PageTemplate>
                 <TodoInput />
+                <TodoList />
             </PageTemplate>
             
         )
